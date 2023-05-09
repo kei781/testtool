10ff8ba7e82628e8f01afd79da9db1a86434a771
known <known@cv-3.com>
2023-05-09 16:09:47 +0900

    대충

---
+++ script.js
@@ -0,0 +1,11 @@
+import http from 'k6/http'
+import { sleep } from 'k6'
+
+export let options = {
+  vus: 10000,
+  duration: '10s'
+}
+export default function () {
+  http.post('http://121.134.241.225:8002/checkInfo')
+  sleep(1)
+}
