847231f4c88c03018b276f6cb24b8360e8fbba11
kei781 <shtkddns5395@gmail.com>
2023-05-09 16:38:52 +0900

    test

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ import http from 'k6/http'
 import { sleep } from 'k6'
 
 export let options = {
-  vus: 200,
+  vus: 1000,
   duration: '10s'
 }
 export default function () {
-  http.post('http://121.134.241.225:8002/checkInfo')
-
+  http.post('http://121.134.241.225/checkInfo')
 }
